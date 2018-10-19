493754dd4aab274eb354bd96a8a68e607e16d460
awadevelop <awadevelop@gmail.com>
2018-10-18 23:41:45 -0300

    Material Component Framework for Vue.js 2

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <c-home></c-home>
+  </v-app>
+</template>
+
+<script>
+import CHome from './components/CHome'
+
+export default {
+  name: 'App',
+  components: {
+    CHome
+  }
+}
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions,
+  VTextField,
+  VDataTable,
+  VCard,
+  VDialog,
+  VDivider,
+  VBadge
+} from 'vuetify'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions,
+    VTextField,
+    VDataTable,
+    VCard,
+    VDialog,
+    VDivider,
+    VBadge
+  },
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  },
+  customProperties: true,
+  iconfont: 'md',
+})
